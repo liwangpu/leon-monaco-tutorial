4557d1e3166d9f085e37c2257ca7dfeb3032ab58
puliwang@cxist.com <puliwang@cxist.com>
2021-07-21 10:29:28 +0800

    修复bom库编译失败问题

--- server/src/my/removeBOM.ts
+++ server/src/my/removeBOM.ts
@@ -1,15 +1,37 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+import * as isUtf8 from 'is-utf8';
 
-import removeBomBuffer = require("strip-bom-buf");
-import removeBomString from "strip-bom";
+function removeBomBuffer(buffer) {
+    if (!Buffer.isBuffer(buffer)) {
+        throw new TypeError(`Expected a Buffer, got ${typeof buffer}`);
+    }
+
+    if (buffer[0] === 0xEF && buffer[1] === 0xBB && buffer[2] === 0xBF && isUtf8(buffer)) {
+        return buffer.slice(3);
+    }
+
+    return buffer;
+};
+
+function stripBom(string) {
+    if (typeof string !== 'string') {
+        throw new TypeError(`Expected a string, got ${typeof string}`);
+    }
+
+    // Catches EFBBBF (UTF-8 BOM) because the buffer-to-string
+    // conversion translates it to FEFF (UTF-16 BOM).
+    if (string.charCodeAt(0) === 0xFEFF) {
+        return string.slice(1);
+    }
+
+    return string;
+}
+
+
 
 export function removeBOMFromBuffer(buffer: Buffer): Buffer {
     return <Buffer>removeBomBuffer(buffer);
 }
 
 export function removeBOMFromString(line: string): string {
-    return removeBomString(line.trim());
+    return stripBom(line.trim());
 }
